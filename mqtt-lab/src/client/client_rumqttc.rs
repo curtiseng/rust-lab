e48c8a56e7988b4317938ca4c62c30a0e984337c
zifeng <zifeng.1024@gmail.com>
2020-11-11 15:55:03 +0800

    mqtt-client.

---
+++ mqtt-lab/src/client/client_rumqttc.rs
@@ -0,0 +1,25 @@
+use rumqttc::{MqttOptions, AsyncClient, QoS};
+use tokio::{task, time};
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let mut mqttoptions = MqttOptions::new("mqtt-async", "127.0.0.1", 1883);
+    mqttoptions.set_keep_alive(5);
+
+    let (client, mut event_loop) = AsyncClient::new(mqttoptions, 10);
+    client.subscribe("world", QoS::AtMostOnce).await.unwrap();
+
+    task::spawn(async move {
+        for i in 0..10 {
+            client.publish("hello", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+            time::delay_for(Duration::from_millis(100)).await;
+        }
+    });
+
+    loop {
+        let notification = event_loop.poll().await.unwrap();
+        println!("Received = {:?}", notification);
+        tokio::time::delay_for(Duration::from_secs(1)).await;
+    }
+}
