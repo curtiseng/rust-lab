7afa22193a622bfa07aa5e4f8417b718c26745e3
zifeng <zifeng.1024@gmail.com>
2020-11-16 16:40:58 +0800

    shared state.

--- mqtt-lab/src/client/client_rumqttc.rs
+++ mqtt-lab/src/client/client_rumqttc.rs
@@ -1,19 +1,25 @@
-use rumqttc::{MqttOptions, AsyncClient, QoS};
+use rumqttc::{MqttOptions, AsyncClient, QoS, LastWill};
 use tokio::{task, time};
 use std::time::Duration;
 
-#[tokio::main]
-async fn main() {
-    let mut mqttoptions = MqttOptions::new("mqtt-async", "127.0.0.1", 1883);
-    mqttoptions.set_keep_alive(5);
-    mqttoptions.set_clean_session(false);
+/// 模拟两个设备
+/// A设备 pub 10条消息, 并订阅
 
-    let (client, mut event_loop) = AsyncClient::new(mqttoptions, 10);
-    client.subscribe("world", QoS::ExactlyOnce).await.unwrap();
+#[tokio::main(max_threads = 3)]
+async fn main() {
+    // 上报客户端
+    let mut client_report_options = MqttOptions::new("mqtt-async", "127.0.0.1", 1883);
+    let will = LastWill::new("topic", QoS::ExactlyOnce, "I'm reporter, good bye");
+    client_report_options.set_keep_alive(5)
+        .set_clean_session(false)
+        .set_last_will(will);
+    let (reporter, mut event_loop) = AsyncClient::new(client_report_options, 10);
+
+    reporter.subscribe("topic", QoS::ExactlyOnce).await.unwrap();
 
     task::spawn(async move {
         for i in 0..10 {
-            client.publish("hello", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+            reporter.publish("topic", QoS::ExactlyOnce, false, String::new()).await.unwrap();
             time::delay_for(Duration::from_millis(100)).await;
         }
     });
--- tokio-lab/src/sharedstate/shared_state.rs
+++ tokio-lab/src/sharedstate/shared_state.rs
@@ -1,30 +1,36 @@
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
 use std::option::Option::Some;
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
-
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-
+        let db = db.clone();
+        println!("Accepted");
         tokio::spawn(
             async move {
-                process(socket).await;
+                process(socket, db).await;
             }
         );
     }
 }
 
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
-
+async fn process(socket: TcpStream, db: Db) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
