3db2268556d0c9013773da8bdddd147f6c98b70e
zifeng <zifeng.1024@gmail.com>
2020-11-17 10:30:23 +0800

    add log for mqtt-lab.

--- mqtt-lab/src/client/client_rumqttc.rs
+++ mqtt-lab/src/client/client_rumqttc.rs
@@ -1,26 +1,30 @@
 use rumqttc::{MqttOptions, AsyncClient, QoS, LastWill};
 use tokio::{task, time};
 use std::time::Duration;
+use log::info;
+use log4rs;
 
 /// 模拟两个设备
 /// A设备 pub 10条消息, 并订阅
 
 #[tokio::main(max_threads = 3)]
 async fn main() {
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     // 上报客户端
     let mut client_report_options = MqttOptions::new("mqtt-async", "127.0.0.1", 1883);
-    let will = LastWill::new("topic", QoS::ExactlyOnce, "I'm reporter, good bye");
+    let will = LastWill::new("topic", QoS::AtMostOnce, "I'm reporter, good bye");
     client_report_options.set_keep_alive(5)
         .set_clean_session(false)
         .set_last_will(will);
     let (reporter, mut event_loop) = AsyncClient::new(client_report_options, 10);
 
-    reporter.subscribe("topic", QoS::ExactlyOnce).await.unwrap();
+    reporter.subscribe("topic", QoS::AtMostOnce).await.unwrap();
 
     task::spawn(async move {
         for i in 0..10 {
-            reporter.publish("topic", QoS::ExactlyOnce, false, String::new()).await.unwrap();
-            time::delay_for(Duration::from_millis(100)).await;
+            info!("publish: {}", i.to_string().as_str());
+            reporter.publish("topic", QoS::AtMostOnce, false, String::from("{\"hello\" : \"world-\"") + &i.to_string() + "}").await.unwrap();
+            time::delay_for(Duration::from_millis(1000)).await;
         }
     });
 
