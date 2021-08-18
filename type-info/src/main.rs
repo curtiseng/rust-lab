451562425694d868d500e13825b7058989fd9236
yangzifeng <yangzifeng@cetccloud.com>
2021-08-18 15:03:46 +0800

    .

--- type-info/src/main.rs
+++ type-info/src/main.rs
@@ -51,6 +51,48 @@ impl<'de> de::Deserialize<'de> for ConnectorTopics {
     }
 }
 
+#[derive(Serialize, Deserialize)]
+struct MyStruct {
+    #[serde(with = "from::string")]
+    biguint: u64,
+
+    #[serde(with = "from::string")]
+    bigint: i64,
+}
+
+// Serde's 'with' attribute allows you to define a serializer and a deserializer in a module.
+// You can implement serialize and deserialize as functions in your code, without using modules,
+// and use the `deserialize_with` and `serialize_with` attributes instead.
+pub mod from {
+    // This is just syntactic sugar, so the attribute looks cool like "from::string".
+    // You don't need to embed multiple modules.
+    pub mod string {
+        use serde::{de::Error as _, Deserialize, Deserializer, Serialize, Serializer};
+
+        // This deserializer was originally written with u64 in mind. Then it was made generic by
+        // changing u64 to T everywhere and adding boundaries. Same with the serializer.
+        pub fn deserialize<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+            where
+                D: Deserializer<'de>,
+                T: std::str::FromStr,
+                <T as std::str::FromStr>::Err: std::fmt::Display,
+        {
+            String::deserialize(deserializer)?
+                .parse::<T>()
+                .map_err(|e| D::Error::custom(format!("{}", e)))
+        }
+
+        pub fn serialize<S, T>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: Serializer,
+                T: std::fmt::Display,
+        {
+            format!("{}", value).serialize(serializer)
+        }
+    }
+}
+
+
 fn main() {
     let input = r#"{
       "test-name": {
